81fec97ce7e8340017d24ab684eb85e1489245a2
Daeyoung Yoon <7rieden@gmail.com>
2020-05-31 04:33:32 +0200

    updated

---
+++ gretting.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".js-form"),
+    input = document.querySelector("input");
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        //she is not
+    } else {
+        paintGreeting(currentUser);
+        //she is
+    }
+
+function init(){
+
+}
+init();
