31ff12e6a31e89d80a657113efc6411b32dbd255
Daeyoung Yoon <7rieden@gmail.com>
2020-06-01 22:46:07 +0200

    updated

--- clock.js
+++ clock.js
@@ -7,10 +7,10 @@ function getTime(){
     const hours = date.getHours();
     const seconds = date.getSeconds();
     // ternary operator(삼항 연산자)
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `${minutes}`: minutes
-    }:${seconds < 10 ? `0${seconds}` : seconds
-    }`;
+    clockTitle.innerText = 
+    `${hours < 10 ? `0${hours}` : hours
+    }:${minutes < 10 ? `0${minutes}`: minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
 
 }
 
--- gretting.js
+++ gretting.js
@@ -1,9 +1,21 @@
 const form = document.querySelector(".js-form"),
-    input = document.querySelector("input");
+    input = document.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("summit", handleSubmit)
+}
+
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
